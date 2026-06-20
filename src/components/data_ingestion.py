d2d665185d585fa763ea148f2d57757f81df55ff
Aryan Dangi <randomone12ar@gmail.com>
2026-06-20 12:18:38 +0530

    Completed model training pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,7 +10,7 @@ from src.logger import logging
 
 from src.components.data_transformation import DataTransformation
 
-
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -87,4 +87,7 @@ if __name__ == "__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
