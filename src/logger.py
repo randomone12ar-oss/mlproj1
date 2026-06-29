193dc57358295a297653e5d67774298df4e4790f
Aryan Dangi <randomone12ar@gmail.com>
2026-06-29 10:49:35 +0530

    Fix Flask console logging output and preprocessor column names mapping

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
@@ -10,9 +11,12 @@ os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ]
 )
 
 if __name__ == "__main__":
