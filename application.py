39c828260242686bcd016752cd15317889c1aaf8
Aryan Dangi <randomone12ar@gmail.com>
2026-07-03 16:27:12 +0530

    Deployment configuration

--- application.py
+++ application.py
@@ -41,8 +41,8 @@ def predict_datapoint():
 
 
 if __name__ == "__main__":
-    app.run(
+    application.run(
         host="127.0.0.1",
-        port=8000,
-        debug=False
+        port=8000
+        
     )
